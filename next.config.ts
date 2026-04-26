bdc9d9002aa12027b452c1b2b3f82866d2af5c73
Alexis Levano <alexisjarenlevanodominguez@gmail.com>
2026-04-26 13:21:58 -0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {}, // Silenciar warning de webpack config
 };
 
 // @ts-ignore - next-pwa no tiene tipos oficiales
